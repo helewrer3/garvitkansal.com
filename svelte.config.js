a2b58033716812aa84b0c78fa78232342d522bf8
Garvit Kansal <helewrer3@Garvits-MacBook-Pro-2.local>
2023-09-10 19:49:44 +0530

    Deploy #5

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,38 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+// import adapter from '@sveltejs/adapter-static';
+// import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	preprocess: [
+// 		preprocess({
+// 			postcss: true
+// 		})
+// 	],
+// 	kit: {
+// 		adapter: adapter({
+// 			pages: 'build',
+// 			assets: 'build',
+// 			fallback: null
+// 		}),
+// 		prerender: {
+// 			entries: [
+// 				'/',
+// 				'/achievements',
+// 				'/education',
+// 				'/projects',
+// 				'/responsibilities',
+// 				'/workExperience'
+// 			]
+// 		}
+// 	}
+// };
+
+// export default config;
+
+import azure from 'svelte-adapter-azure-swa';
+
+export default {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		prerender: {
-			entries: [
-				'/',
-				'/achievements',
-				'/education',
-				'/projects',
-				'/responsibilities',
-				'/workExperience'
-			]
-		}
+		adapter: azure()
 	}
 };
-
-export default config;
